526de6b821728fe6d958368b2647c4852f191a68
marielaamm <105139668+marielaamm@users.noreply.github.com>
2022-09-10 12:03:41 -0600

    Se programo mensajes de error

--- src/app/main/cat/components/municipio/municipio.component.ts
+++ src/app/main/cat/components/municipio/municipio.component.ts
@@ -3,6 +3,8 @@ import { MatDialog } from '@angular/material/dialog';
 import { ServerService } from 'src/app/main/shared/service/server.service';
 import { iDepartamento } from '../../interface/i-departamento';
 import { CatalogoService } from '../../service/catalogo.service';
+import { Validacion } from 'src/app/main/shared/class/validacion';
+import { DialogoComponent } from 'src/app/main/shared/components/dialogo/dialogo.component';
 
 @Component({
   selector: 'app-municipio',
@@ -10,14 +12,21 @@ import { CatalogoService } from '../../service/catalogo.service';
   styleUrls: ['./municipio.component.scss']
 })
 export class MunicipioComponent implements OnInit {
+  public val : Validacion = new Validacion();
 
   public lstDepartamento: iDepartamento [] = [];
   private _CatalogoService : CatalogoService;
 
   constructor(private ServerScv : ServerService, private _Dialog: MatDialog) {
+    this.val.add("txtMunicipio","1","LEN>", "0");
+    this.val.add("txtMunicipio", "2","LEN<=", "50");
+    this.val.add("txtDepartamento", "1", "LEN>", "0");
+
     this._CatalogoService = new CatalogoService(this._Dialog)
     this._CatalogoService.BuscarDpto("");
    }
+
+
 
 
   private LlenarDpto(datos:string):void{
@@ -38,6 +47,37 @@ export class MunicipioComponent implements OnInit {
         event.newSelection = event.added;
     }
 }
+
+
+
+public v_Guardar():void{
+  let esError :string="";
+  let mensaje :string="<ol>";
+
+  if(this.val.ValForm.get("txtMunicipio")?.invalid){
+    mensaje += "<li>Ingrese el nombre del Municipio o revise la cantidada de caracteres</li>";
+    esError +="1";
+  }
+
+  if(this.val.ValForm.get("txtDepartamento")?.invalid){
+    mensaje += "<li>Seleccione un departamento</li>"
+    esError +="1";
+  }
+  mensaje +="</ol>";
+
+  if(esError.includes("1")){
+    let s : string = "{ \"d\":  [{ }],  \"msj\": " + "{\"Codigo\":\""+ 1 + "\",\"Mensaje\":\""+ mensaje + "\"}"+ ", \"count\":"+ 0 + ", \"esError\":"+ 1 + "}";
+
+    let _json= JSON.parse(s);
+    this._Dialog.open(DialogoComponent,{
+      data: _json["msj"]
+    });
+    return;
+
+
+  }
+}
+
 
   Cerrar() : void {
 
