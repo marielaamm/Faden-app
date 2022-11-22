e58ad45d711e780ffd83bc0868ca508dfff2f444
carolfp1974 <carolflorespe@gmail.com>
2022-11-21 22:41:12 -0600

    consensomedico html

--- src/app/main/inicio/components/consensomedico/consensomedico.component.ts
+++ src/app/main/inicio/components/consensomedico/consensomedico.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CatalogoService } from 'src/app/main/cat/service/catalogo.service';
+import { Validacion } from 'src/app/main/shared/class/validacion';
 import { ServerService } from 'src/app/main/shared/service/server.service';
 
 @Component({
@@ -9,8 +11,22 @@ import { ServerService } from 'src/app/main/shared/service/server.service';
 export class ConsensomedicoComponent implements OnInit {
 
   public isLinear = false;
+  public val: Validacion = new Validacion();
+  private _CatalogoService: CatalogoService;
   
-  constructor(private ServerScv : ServerService) { }
+  constructor(private ServerScv : ServerService) { 
+
+    this.val.add("rdNormal","1", "LEN>=", "0");
+    this.val.add("rdLeve","1", "LEN>=", "0");
+    this.val.add("rdMayor","1", "LEN>=", "0");
+    this.val.add("chkDepre","1", "LEN>=", "0");
+    this.val.add("chkBipolar","1", "LEN>=", "0");
+    this.val.add("chkEsquizo","1", "LEN>=", "0");
+    this.val.add("chkOtro","1", "LEN>=", "0");
+    this.val.add("rdProbable","1", "LEN>=", "0");
+    this.val.add("rdConfir","1", "LEN>=", "0");
+
+  }
   
   public singleSelection(event: any) {
     if (event.added.length) {
