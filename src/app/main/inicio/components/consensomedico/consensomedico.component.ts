6b6fba49c96682e33489e31d3a8b85a2b50c415d
carolfp1974 <carolflorespe@gmail.com>
2023-02-08 22:43:14 -0600

    consenso medico ts

--- src/app/main/inicio/components/consensomedico/consensomedico.component.ts
+++ src/app/main/inicio/components/consensomedico/consensomedico.component.ts
@@ -25,25 +25,25 @@ export class ConsensomedicoComponent implements OnInit {
   constructor(private ServerScv : ServerService, private _Dialog: MatDialog) { 
 
     //[ REGLAS]
-    this.val.add("rdNormal","1", "LEN>", "0");
+    this.val.add("rdNormal", "1", "NUM>", "0");
     this.val.add("txtDetNormal","1", "LEN>", "0");
-    this.val.add("rdLeve","1", "LEN>", "0");
+    this.val.add("rdLeve", "1", "NUM>", "0");
     this.val.add("txtDetLeve","1", "LEN>", "0");
-    this.val.add("rdMayor","1", "LEN>", "0");
+    this.val.add("rdMayor", "1", "NUM>", "0");
     this.val.add("txtDetMayor","1", "LEN>", "0");    
 
-    this.val.add("chkDepre","1", "LEN>", "0");
-    this.val.add("txtDepresion","1", "LEN>", "0");
-    this.val.add("chkBipolar","1", "LEN>", "0");
-    this.val.add("txtBipolar","1", "LEN>", "0");
-    this.val.add("chkEsquizo","1", "LEN>", "0");
-    this.val.add("txtEsquizo","1", "LEN>", "0");
-    this.val.add("chkOtroDiag","1", "LEN>", "0");
-    this.val.add("txtOtroDiag","1", "LEN>", "0");
+    this.val.add("chkDepre", "1", "LEN>", "0");
+    this.val.add("txtDepresion", "1", "LEN>", "0");
+    this.val.add("chkBipolar", "1", "LEN>", "0");
+    this.val.add("txtBipolar", "1", "LEN>", "0");
+    this.val.add("chkEsquizo", "1", "LEN>", "0");
+    this.val.add("txtEsquizo", "1", "LEN>", "0");
+    this.val.add("chkOtroDiag", "1", "LEN>", "0");
+    this.val.add("txtOtroDiag", "1", "LEN>", "0");
 
-    this.val.add("rdProbable","1", "LEN>", "0");
+    this.val.add("rdProbable","1", "NUM>", "0");
     this.val.add("txtDiagProbable","1", "LEN>", "0");
-    this.val.add("rdConfir","1", "LEN>", "0");
+    this.val.add("rdConfir", "1", "NUM>", "0");
     this.val.add("txtDiagConfir","1", "LEN>", "0");
 
     this.val.add("txtTraFarma","1", "LEN>","0");
@@ -51,41 +51,70 @@ export class ConsensomedicoComponent implements OnInit {
     this.val.add("txtRecomendaciones","1", "LEN>","0");
     this.val.add("txtExamenes","1", "LEN>","0");
 
+    this._ExpdienteService = new ExpdienteService(this._Dialog);
+
     this.limpiar();
 
-    this._ExpdienteService = new ExpdienteService(this._Dialog);
+   
 
   }
 
   public limpiar(){
-    this.val.ValForm.get("rdNormal")?.setValue("");
+    this.val.ValForm.get("rdNormal")?.setValue(false);
     this.val.ValForm.get("txtDetNormal")?.setValue("");
-    this.val.ValForm.get("rdLeve")?.setValue("");
+    this.val.ValForm.get("txtDetNormal")?.disable();
+
+    this.val.ValForm.get("rdLeve")?.setValue(false);
     this.val.ValForm.get("txtDetLeve")?.setValue("");
-    this.val.ValForm.get("rdMayor")?.setValue("");
+    this.val.ValForm.get("txtDetLeve")?.disable();
+
+    this.val.ValForm.get("rdMayor")?.setValue(false);
     this.val.ValForm.get("txtDetMayor")?.setValue("");
-    this.val.ValForm.get("chkDepre")?.setValue("");
+    this.val.ValForm.get("txtDetMayor")?.disable();
+
+
+    this.val.ValForm.get("chkDepre")?.setValue(false);
     this.val.ValForm.get("txtDepresion")?.setValue("");
-    this.val.ValForm.get("chkBipolar")?.setValue("");
+    this.val.ValForm.get("txtDepresion")?.disable();
+    
+    this.val.ValForm.get("chkBipolar")?.setValue(false);
     this.val.ValForm.get("txtBipolar")?.setValue("");
-    this.val.ValForm.get("chkEsquizo")?.setValue("");
+    this.val.ValForm.get("txtBipolar")?.disable();
+    
+
+    this.val.ValForm.get("chkEsquizo")?.setValue(false);
     this.val.ValForm.get("txtEsquizo")?.setValue("");
-    this.val.ValForm.get("chkOtroDiag")?.setValue("");
+    this.val.ValForm.get("txtEsquizo")?.disable();
+
+
+    this.val.ValForm.get("chkOtroDiag")?.setValue(false);
     this.val.ValForm.get("txtOtroDiag")?.setValue("");
+    this.val.ValForm.get("txtOtroDiag")?.disable();
+
+
     this.val.ValForm.get("rdProbable")?.setValue("");
     this.val.ValForm.get("txtDiagProbable")?.setValue("");
+    this.val.ValForm.get("txtDiagProbable")?.disable();
+
+
     this.val.ValForm.get("rdConfir")?.setValue("");
     this.val.ValForm.get("txtDiagConfir")?.setValue("");
+    this.val.ValForm.get("txtDiagConfir")?.disable();
+
     this.val.ValForm.get("txtTraFarma")?.setValue("");
     this.val.ValForm.get("txtTraNoFarma")?.setValue("");
     this.val.ValForm.get("txtRecomendaciones")?.setValue("");
     this.val.ValForm.get("txtExamenes")?.setValue("");
+
+    
 
     if(this.Sindrome != undefined)
     {
       this.Sindrome?.dataSource.data.splice(0, this.Sindrome.dataSource.data.length);
       this.Sindrome.dataSource.filter = "";
     }
+
+    this._ExpdienteService = new ExpdienteService(this._Dialog);
 
   }
   
@@ -144,9 +173,30 @@ public f_RefOtroDiag(): void {
   }
 }
 
-
 //*************** */
 
+public Guardar (){
+
+  let esError: string = " ";
+  let mensaje: string = "<ol>";
+
+  //[Inicio - Validaciones]
+
+  if (this.val.ValForm.get("txtDetNormal")?.invalid) {
+    mensaje += "<li>Describa el nivel de deterioro o revise la cantidad de caracteres</li>";
+    esError += "1";
+  }
+
+  if (this.val.ValForm.get("txtDetLeve")?.invalid) {
+    mensaje += "<li>Describa el nivel de deterioro o revise la cantidad de caracteres</li>";
+    esError += "1";
+  }
+
+  if (this.val.ValForm.get("txtDetMayor")?.invalid) {
+    mensaje += "<li>Describa el nivel de deterioro o revise la cantidad de caracteres</li>";
+    esError += "1";
+  }
+}
 
    Cerrar() : void{
     
