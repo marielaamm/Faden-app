e695651d9cb205bb4384774392f5fcf64781ab30
carolfp1974 <carolflorespe@gmail.com>
2023-03-08 22:31:15 -0600

    soap component ts

--- src/app/main/inicio/components/soap/soap.component.ts
+++ src/app/main/inicio/components/soap/soap.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { CatalogoService } from 'src/app/main/cat/service/catalogo.service';
+import { Validacion } from 'src/app/main/shared/class/validacion';
 import { ServerService } from 'src/app/main/shared/service/server.service';
+import { ExpdienteService } from '../../service/expediente.service';
 
 @Component({
   selector: 'app-soap',
@@ -10,8 +14,19 @@ export class SoapComponent implements OnInit {
 
   public isLinear = false;
   public lstPaciente:{}[]=[];
+  public val: Validacion = new Validacion ();
+  
+  private _CatalogoService: CatalogoService;
+  private _ExpdienteService: ExpdienteService;
 
-  constructor(private ServerScv : ServerService) { }
+  constructor(private ServerScv : ServerService, private _Dialog: MatDialog) {
+   // [ REGLAS ] 
+
+   this.val.add("txtFecha", "1", "LEN>","0");
+   this.val.add("txtHora", "1", "LEN>", "0");
+
+   }
+
   public singleSelection(event: any) {
     if (event.added.length) {
         event.newSelection = event.added;
---
+++ src/app/main/inicio/interface/i-sistema-soap.ts
@@ -0,0 +1,17 @@
+export interface iSistemaSoap{
+    IdSoap: Number,
+    Fecha: Date,
+    Hora: Date,
+    NombrePaciente: String,
+    NoExpediente: Number,
+    TipoAcompanante: Number,
+    NombreAcompañante: String,
+    Direccion: String,
+    Telefono: String,
+    PropositoVisita: Number,
+    Subjetivo: String,
+    Objetivo: String,
+    Avaluo: String,
+    Planes: String,
+    IdPaciente: Number
+}
